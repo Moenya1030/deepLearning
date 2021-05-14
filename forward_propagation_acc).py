daaffa30394cab4582155a70f0ac896778f5fe1b
Moenya1030 <52889062+Moenya1030@users.noreply.github.com>
2021-05-14 11:16:07 +0800

    Create forward_propagation_acc).py

---
+++ forward_propagation_acc).py
@@ -0,0 +1,121 @@
+# 在前向传播的基础上增加了一个测试的部分
+import  tensorflow as tf
+from    tensorflow import keras
+from    tensorflow.keras import datasets
+import  os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# x: [60k, 28, 28], [10k, 28, 28]
+# y: [60k], [10k]
+(x, y), (x_test, y_test) = datasets.mnist.load_data()
+# x: [0~255] => [0~1.]
+x = tf.convert_to_tensor(x, dtype=tf.float32) / 255.
+y = tf.convert_to_tensor(y, dtype=tf.int32)
+x_test = tf.convert_to_tensor(x_test, dtype=tf.float32) / 255.
+y_test = tf.convert_to_tensor(y_test, dtype=tf.int32)
+
+print(x.shape, y.shape, x.dtype, y.dtype)
+print(tf.reduce_min(x), tf.reduce_max(x))
+print(tf.reduce_min(y), tf.reduce_max(y))
+
+
+train_db = tf.data.Dataset.from_tensor_slices((x,y)).batch(128)
+test_db = tf.data.Dataset.from_tensor_slices((x_test,y_test)).batch(128)
+train_iter = iter(train_db)
+sample = next(train_iter)
+print('batch:', sample[0].shape, sample[1].shape)
+
+
+# [b, 784] => [b, 256] => [b, 128] => [b, 10]
+# [dim_in, dim_out], [dim_out]
+w1 = tf.Variable(tf.random.truncated_normal([784, 256], stddev=0.1))
+b1 = tf.Variable(tf.zeros([256]))
+w2 = tf.Variable(tf.random.truncated_normal([256, 128], stddev=0.1))
+b2 = tf.Variable(tf.zeros([128]))
+w3 = tf.Variable(tf.random.truncated_normal([128, 10], stddev=0.1))
+b3 = tf.Variable(tf.zeros([10]))
+
+lr = 1e-3
+
+for epoch in range(100): # iterate db for 10
+    for step, (x, y) in enumerate(train_db): # for every batch
+        # x:[128, 28, 28]
+        # y: [128]
+
+        # [b, 28, 28] => [b, 28*28]
+        x = tf.reshape(x, [-1, 28*28])
+
+        with tf.GradientTape() as tape: # tf.Variable
+            # x: [b, 28*28]
+            # h1 = x@w1 + b1
+            # [b, 784]@[784, 256] + [256] => [b, 256] + [256] => [b, 256] + [b, 256]
+            h1 = x@w1 + tf.broadcast_to(b1, [x.shape[0], 256])
+            h1 = tf.nn.relu(h1)
+            # [b, 256] => [b, 128]
+            h2 = h1@w2 + b2
+            h2 = tf.nn.relu(h2)
+            # [b, 128] => [b, 10]
+            out = h2@w3 + b3
+
+            # compute loss
+            # out: [b, 10]
+            # y: [b] => [b, 10]
+            y_onehot = tf.one_hot(y, depth=10)
+
+            # mse = mean(sum(y-out)^2)
+            # [b, 10]
+            loss = tf.square(y_onehot - out)
+            # mean: scalar
+            loss = tf.reduce_mean(loss)
+
+        # compute gradients
+        grads = tape.gradient(loss, [w1, b1, w2, b2, w3, b3])
+        # print(grads)
+        # w1 = w1 - lr * w1_grad
+        w1.assign_sub(lr * grads[0])
+        b1.assign_sub(lr * grads[1])
+        w2.assign_sub(lr * grads[2])
+        b2.assign_sub(lr * grads[3])
+        w3.assign_sub(lr * grads[4])
+        b3.assign_sub(lr * grads[5])
+
+
+        if step % 100 == 0:
+            print(epoch, step, 'loss:', float(loss))
+
+
+    # test/evluation
+    # [w1, b1, w2, b2, w3, b3]
+    total_correct, total_num = 0, 0
+    for step, (x,y) in enumerate(test_db): # 对数据集还得进行迭代一次，不能再用train_db进行迭代，不然可能造成数据泄露
+
+        # [b, 28, 28] => [b, 28*28]
+        x = tf.reshape(x, [-1, 28*28])
+
+        # 前向传播
+        # [b, 784] => [b, 256] => [b, 128] => [b, 10]
+        h1 = tf.nn.relu(x@w1 + b1)
+        h2 = tf.nn.relu(h1@w2 + b2)
+        out = h2@w3 +b3 # 不加激活函数
+
+        # out: [b, 10] ~ R
+        # 概率 prob: [b, 10] ~ [0, 1]
+        prob = tf.nn.softmax(out, axis=1) # softmax
+
+        # [b, 10] => [b]
+        # int64!!!
+        pred = tf.argmax(prob, axis=1) # 选取概率最大值所在的索引
+        pred = tf.cast(pred, dtype=tf.int32) # 记得转换为int32，或者其他地方转换为int64
+
+        # y: [b]
+        # [b], int32
+        # print(pred.dtype, y.dtype) ==》 ([b], int64),  ([b], int32)
+        correct = tf.cast(tf.equal(pred, y), dtype=tf.int32) # 把equal的布尔型转换为int
+        correct = tf.reduce_sum(correct) # 统计预测正确的个数
+
+        total_correct += int(correct)
+        total_num += x.shape[0] # 总测试数量
+
+    acc = total_correct / total_num
+    print('test acc:', acc)
